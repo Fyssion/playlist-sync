876ffa7ace5cda988fd406e85f4268e8f248fe0c
Fyssion <fyssioncodes@gmail.com>
2024-10-17 12:32:55 -0500

    Export Playlist and Track at module level

--- playlist_sync/__init__.py
+++ playlist_sync/__init__.py
@@ -1,3 +1,6 @@
 # SPDX-FileCopyrightText: 2024-present Fyssion <fyssioncodes@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+
+from .playlist import Playlist as Playlist
+from .track import Track as Track
